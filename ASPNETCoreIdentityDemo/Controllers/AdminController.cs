4714d476374b3e1c3cb639a0c990c990345d77c5
Muhammad Uzair Aslam <Uzair.Aslam@giantgroup.com>
2024-08-13 19:08:25 +0500

    Roles Management

---
+++ ASPNETCoreIdentityDemo/Controllers/AdminController.cs
@@ -0,0 +1,129 @@
+﻿using ASPNETCoreIdentityDemo.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETCoreIdentityDemo.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdminController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel roleModel) 
+        {
+            if (ModelState.IsValid)
+            {
+                bool roleExist = await _roleManager.RoleExistsAsync(roleModel.RoleName);
+                if (roleExist)
+                {
+                    ModelState.AddModelError("", "Role Already Exist.");
+                }
+                else
+                {
+                    var role = new IdentityRole() { Name = roleModel?.RoleName };
+                    IdentityResult result = await _roleManager.CreateAsync(role);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("ListRoles");
+                    }
+
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(roleModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListRoles()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            return View(roles);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string roleId)
+        {
+            var role = await _roleManager.FindByIdAsync(roleId);
+            if (role == null)
+            {
+                return View("Error");
+            }
+            var roleModel = new EditRoleViewModel()
+            {
+                Id = role.Id,
+                RoleName = role.Name
+            };
+            return View(roleModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = await _roleManager.FindByIdAsync(model.Id);
+                if (role == null)
+                {
+                    ViewBag.ErrorMessage = $"Role with Id = {model.Id} cannot be found";
+                    return View("NotFound");
+                }
+                else
+                {
+                    role.Name = model.RoleName;
+                     var result = await _roleManager.UpdateAsync(role);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("ListRoles");
+                    }
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                    return View(model);
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteRole(string roleId)
+        {
+            var role = await _roleManager.FindByIdAsync(roleId);
+            if (role == null)
+            {
+                // Role not found, handle accordingly
+                ViewBag.ErrorMessage = $"Role with Id = {roleId} cannot be found";
+                return View("NotFound");
+            }
+            var result = await _roleManager.DeleteAsync(role);
+            if (result.Succeeded)
+            {
+                // Role deletion successful
+                return RedirectToAction("ListRoles"); // Redirect to the roles list page
+            }
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+            // If we reach here, something went wrong, return to the view
+            return View("ListRoles", await _roleManager.Roles.ToListAsync());
+        }
+    }
+}
---
+++ ASPNETCoreIdentityDemo/Models/ViewModels/EditRoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPNETCoreIdentityDemo.Models.ViewModels
+{
+    public class EditRoleViewModel
+    {
+        [Required]
+        public string Id { get; set; }
+        [Required(ErrorMessage = "Role Name is Required")]
+        public string RoleName { get; set; }
+    }
+}
